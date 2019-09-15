c240c43ec4c84f224181b3a72256c890f8b4c6b1
Ahmad NIOMIC <ahmad.umb15@gmail.com>
2019-09-15 15:28:32 +0700

    tugas-js-3

---
+++ src/Page/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TopEvents from '../Events/TopEvents';
+
+const Footer = (props) => {
+  return (
+    <div>
+        <TopEvents />
+        <h4>Tugas 3 React JS</h4>
+    </div>
+  );
+};
+
+export default Footer;
